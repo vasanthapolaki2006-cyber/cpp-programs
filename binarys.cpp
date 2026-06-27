1da991874e89eedcadca5c85a968a3b1cf5b5fe0
vasanthapolaki2006-cyber <vasanthapolaki2006@gmail.com>
2026-06-27 14:36:19 +0530

    Update binarys.cpp

--- binarys.cpp
+++ binarys.cpp
@@ -1,36 +1,37 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int main()
 {
-  int n,key,mid;
-  cin>>n;
-  cout<<"enter the key value";
-  cin>>key;
-  int l=0,h=n-1;
-  if(l<h)
-  {
-      mid=(l+h)/2;
-  }
-  cout<<"array contains";
-  int a[n];
-  for(int i=0;i<n;i++)
-  cin>>a[i];
-  if(a[mid]<key)
-  {
-      h=mid-1;
-  }
-  else
-  {
-      l=mid+1;
-  }
-  if(a[mid]==key)
-   {
-      cout<<"key value found at index  "<<mid;
-  }
-  else
-  {
-      cout<<"key vaue is not found";
-  }
+    int n,key,flag = 0;
+    cin>>n;
+    int a[n];
+    cout<<"Enter array elements: ";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    cout<<"Enter key value: ";
+    cin>>key;
+    int l=0,h=n-1;
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(a[mid]==key)
+        {
+            cout<<"Key value found at index "<<mid;
+            flag=1;
+            break;
+        }
+        else if(a[mid]<key)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            h=mid-1;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"Key value not found";
+    }
     return 0;
 }
